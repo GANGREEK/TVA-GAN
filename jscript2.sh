c8ba872fc302ca4c58bf6b9456c33b1e3f845b8f
GAN_GREEK <nandkmyadav@gmail.com>
2022-11-03 12:30:58 +0545

    Add files via upload

---
+++ jscript2.sh
@@ -0,0 +1,3 @@
+
+python3 train.py --dataroot ./datasets/WHU  --name WHUALSYNF2 --model TVAGANModel --no_dropout --gpu_ids 1  --display_id 0 --dataset_mode aligned 
+
---
+++ test_.py
@@ -0,0 +1,5 @@
+
+
+
+while True:
+    pass
